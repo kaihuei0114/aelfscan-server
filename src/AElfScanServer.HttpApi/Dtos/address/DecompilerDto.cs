ab62c9738dd29281744d77c2b74c7cf3db9266c4
alanexplore <alanexplore@aelfscan.io>
2024-08-07 15:19:14 +0800

    Fix DecompilerContractDto

--- src/AElfScanServer.HttpApi/Dtos/address/DecompilerDto.cs
+++ src/AElfScanServer.HttpApi/Dtos/address/DecompilerDto.cs
@@ -13,6 +13,8 @@ public class GetContractFilesResponseDto
 public class DecompilerContractDto
 {
     public string Name { get; set; }
+    public string Content { get; set; }
+    public string FileType { get; set; }
     public List<DecompilerContractFileDto> Files { get; set; }
 }
 
