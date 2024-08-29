aeac1b1553937b3eb653b233bbe3376f575a575d
alanexplore <alanexplore@aelfscan.io>
2024-08-29 14:55:12 +0800

    Optimize contract address and eoa address search judgment

--- src/AElfScanServer.HttpApi/Service/ChartDataService.cs
+++ src/AElfScanServer.HttpApi/Service/ChartDataService.cs
@@ -474,7 +474,11 @@ public class ChartDataService : AbpRedisCache, IChartDataService, ITransientDepe
 
             var curTotalStaked = curtBpStaked + curtVoteStaked;
 
-            datList[i].Rate = (curTotalStaked / double.Parse(datList[i].Supply) * 100).ToString("F4");
+            if (!datList[i].Supply.IsNullOrEmpty() || datList[i].Supply != "0")
+            {
+                datList[i].Rate = (curTotalStaked / double.Parse(datList[i].Supply) * 100).ToString("F4");
+            }
+          
             datList[i].TotalStaked = curTotalStaked.ToString("f4");
         }
 
